22f4cb9f50177644456574aef4497a2f8c52f420
estudante1977 <antonio-jairo@hotmail.com>
2024-10-12 16:28:40 -0300

    Código python raiz_quadrada.py adicionado.

---
+++ raiz_quadrada.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+x = int(input("Digite o número:  "))
+
+def raiz_quadrada(x):
+    return x ** 0.5
+
+result = raiz_quadrada(x)
+
+print(f"A raiz quadrada de {x} é {result}")
